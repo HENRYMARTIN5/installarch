80331dc915c0b8419ffdc59bfa8b86bdc3f5af30
HENRYMARTIN5 <62612165+HENRYMARTIN5@users.noreply.github.com>
2022-12-01 14:32:59 +0000

    Moved chroot scripts to seperate file

---
+++ chroot.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+TIMEZONE=$1
+HOSTNAME=$2
+USERNAME=$3
+
+# set the timezone
+ln -sf $TIMEZONE /etc/localtime
+hwclock --systohc
+
+# set the hostname
+echo $HOSTNAME > /etc/hostname
+
+# hostfile
+echo "
+# IPv4
+127.0.0.1	localhost
+127.0.1.1	$HOSTNAME.local $HOSTNAME
+
+# IPv6
+::1		localhost" > /etc/hosts
+
+clear && echo "Please enter your root password twice (it will not be displayed), then press enter after each time."
+
+# set root password
+passwd
+
+# make a new user and set its password
+useradd -m $USERNAME
+
+clear && echo "Please enter your user password twice (it will not be displayed), then press enter after each time."
+
+passwd $USERNAME
+
+usermod -aG wheel,audio,video,optical,storage $USERNAME
+
+# uncomment wheel group in sudoers
+sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /etc/sudoers
+
+# Install grub
+pacman -S grub efibootmgr os-prober freetype2 dosfstools --noconfirm > /dev/null
+grub-install --target=x86_64-efi --bootloader-id=grub_uefi
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# install network tools
+pacman -S dhcpcd net-tools netctl dialog wpa_supplicant networkmanager nm-connection-editor inetutils ifplugd --noconfirm
+
+# enable services
+systemctl enable {dhcpcd,NetworkManager,ifplugd}
+
+exit
