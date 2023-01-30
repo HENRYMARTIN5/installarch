3083cd4e584b9939aac453dcecc5eeb2114c781f
HENRYMARTIN5 <62612165+HENRYMARTIN5@users.noreply.github.com>
2023-01-30 12:38:47 -0600

    Install git, curl, and wget automatically

--- chroot.sh
+++ chroot.sh
@@ -39,3 +39,6 @@ pacman -S dhcpcd net-tools netctl dialog wpa_supplicant networkmanager nm-connec
 
 # enable services
 systemctl enable {dhcpcd,NetworkManager,ifplugd}
+
+# install git and other utilities
+pacman -S curl wget git
